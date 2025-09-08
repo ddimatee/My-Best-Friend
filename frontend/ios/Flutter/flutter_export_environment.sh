c3142eaa0a59b84569a71d41016b43751cdfc377
ddimatee <brayandimate2023@gmail.com>
2025-09-07 19:56:56 -0500

    cambios genéricos

--- frontend/ios/Flutter/flutter_export_environment.sh
+++ frontend/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\Braya\dev\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\proyecto\frontend"
+export "FLUTTER_ROOT=C:\Users\Braya\src\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\proyecto\My-Best-Friend\frontend"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
