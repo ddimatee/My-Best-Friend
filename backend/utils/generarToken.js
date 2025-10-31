8ec1efbedc0d678ffaa341490ebdb2f2d21038e2
username <brayandimate2023@gmail.com>
2025-10-31 07:32:46 -0500

    I drop database because want create correctly

--- backend/utils/generarToken.js
+++
@@ -1,9 +0,0 @@
-const jwt = require('jsonwebtoken');
-
-const generarToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: '7d'
-  });
-};
-
-module.exports = generarToken;
